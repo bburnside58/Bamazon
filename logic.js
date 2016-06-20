51c2fac19169ce433fbc58f13be97629ac0ad84c
Brian Burnside <bcburnside58@gmail.com>
2016-06-20 14:28:25 -0400

    selectItem and selectQuantity functions complete, app working as expected

--- logic.js
+++ logic.js
@@ -24,7 +24,7 @@ var selectItem = function() {
     inquirer.prompt([{
         name: "ID",
         type: "input",
-        message: "Choose product ID you would like",
+        message: "Choose product ID you would like:",
     }]).then(function(answer) {
         var query = 'SELECT * FROM Products WHERE ?';
         connection.query(query, {ItemID: answer.ID}, function(err, res) {
@@ -32,7 +32,6 @@ var selectItem = function() {
         		console.log("Insufficient quantity");
         	} else {
         		console.log(res);
-        		console.log(answer.ID);
         		var itemChosen = answer.ID;
         		selectQuantity(itemChosen);
         	}
@@ -47,24 +46,25 @@ var selectQuantity = function(itemChosen) {
         type: "input",
         message: "How many would you like?",
     }]).then(function(answer) {
-    	console.log("this is " + itemChosen);
-        // var query = 'SELECT * FROM Products WHERE ItemID=?', itemChosen,;
         connection.query('SELECT * FROM Products WHERE ItemID=?', itemChosen, function(err, res) {
         	if (res[0].StockQuantity < answer.quantity) {
         		console.log("Insufficient quantity");
         	} else {
-        		console.log("yay me");
+        		
+        		var totalCost = (answer.quantity * res[0].Price).toFixed(2);
+        		console.log("Your total comes out to be $" + totalCost);
+        		var quantityChosen = answer.quantity;
+        		connection.query('UPDATE Products SET StockQuantity = "'+(res[0].StockQuantity - quantityChosen)+'" WHERE ItemID = "'+itemChosen+'"');
+        		connection.query('SELECT * FROM Products WHERE ItemID=?', itemChosen,function(err, res){
+        			console.log("The store now has an inventory of " + res[0].StockQuantity + " for " + res[0].ProductName);
+        			selectItem();
+        		})   		
         	}
-        })
+        })    
     })
 };
-/*
-, {
-        name: "quantity",
-        type: "input",
-        message: "How many would you like?",
-    }
-*/
+
+
 
 
 
