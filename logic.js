db4510958815fef298782c72cf2e8cc052672f13
Brian Burnside <bcburnside58@gmail.com>
2016-06-20 12:01:50 -0400

    started inquirer prompt

--- logic.js
+++ logic.js
@@ -12,3 +12,48 @@ connection.connect(function(err) {
 	if (err) throw err;
 	console.log("connected");
 })
+
+connection.query('SELECT * FROM Products', function(err, res){
+			console.log(res);
+			selectItem();
+})
+
+
+//look into separating these 2 questions
+var selectItem = function() {
+    inquirer.prompt([{
+        name: "ID",
+        type: "input",
+        message: "Choose product ID",
+    }]).then(function(answer) {
+        var query = 'SELECT * FROM Products WHERE ?';
+        connection.query(query, {ItemID: answer.ID}, function(err, res) {
+        	console.log(res);
+            // for (var i = 0; i < res.length; i++) {
+            // 	console.log("Hi");
+            //     // console.log("Position: " + res[i].position + " || Song: " + res[i].song + " || Artist: " + res[i].artist + " || Year: " + res[i].year);
+            // }
+        })
+    })
+};
+
+/*
+, {
+        name: "quantity",
+        type: "input",
+        message: "How many would you like?",
+    }
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
